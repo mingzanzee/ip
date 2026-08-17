736847da852e11da6535c2e1e2043222bba4c9c5
Zee Ming Zan <mingzan0210@gmail.com>
2026-08-17 14:01:11 +0800

    Updated to echo commands

--- src/main/java/Tard_T.java
+++ src/main/java/Tard_T.java
@@ -1,12 +1,36 @@
+import java.util.Scanner;
+
 public class Tard_T {
     public static void main(String[] args) {
         String intro = "Hello! I'm Tard_T. \n" +
                 "What can I do for you? \n" +
-                "________________________________";
+                "________________________________\n";
         System.out.println(intro);
 
-        String exit = "Bye. Hope to see you again soon! \n" +
-                "________________________________";
-        System.out.println(exit);
+        Scanner scanner = new Scanner(System.in);
+
+        // keep accepting inputs until user types "bye"
+        // some customization
+        int command_count = 0;
+        while (true) {
+            String userInput = scanner.nextLine();
+            command_count++;
+
+            if (userInput.equals("bye")) {
+                String exit = "Bye. Hope to see you again soon! \n" +
+                        "________________________________";
+                System.out.println(exit);
+                break;
+            } else if (command_count > 5) {
+                String lazy_echo = "___________________________________\n" + "    " + userInput + "\n        Tired...\n" + "___________________________________";
+                System.out.println(lazy_echo);
+            } else {
+                String echo = "___________________________________\n" + "    " + userInput + "\n___________________________________";
+                System.out.println(echo);
+            }
+        }
+
+
+
     }
 }
