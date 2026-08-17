f8ceb597af16ef22cf5a9bf504a097b240015753
Zee Ming Zan <mingzan0210@gmail.com>
2026-08-17 16:46:56 +0800

    Implemented add and list functions
    Saved the lines to a variable for easier reference and usage

--- src/main/java/Tard_T.java
+++ src/main/java/Tard_T.java
@@ -1,36 +1,49 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Tard_T {
     public static void main(String[] args) {
+        final String LINE = "________________________________";
         String intro = "Hello! I'm Tard_T. \n" +
                 "What can I do for you? \n" +
-                "________________________________\n";
+                LINE + "\n";
         System.out.println(intro);
 
         Scanner scanner = new Scanner(System.in);
 
         // keep accepting inputs until user types "bye"
         // some customization
-        int command_count = 0;
+        int commandCount = 0;
+
+        List<String> taskList = new ArrayList<>();
+
         while (true) {
             String userInput = scanner.nextLine();
-            command_count++;
+            commandCount++;
 
             if (userInput.equals("bye")) {
                 String exit = "Bye. Hope to see you again soon! \n" +
-                        "________________________________";
+                        LINE;
                 System.out.println(exit);
                 break;
-            } else if (command_count > 5) {
-                String lazy_echo = "___________________________________\n" + "    " + userInput + "\n        Tired...\n" + "___________________________________";
-                System.out.println(lazy_echo);
+            } else if (userInput.equals("list")) {
+                int num = 1;
+                System.out.println(LINE + "\n");
+                for (String task : taskList) {
+                    System.out.println(num + ". " + task);
+                    num += 1;
+                }
+                System.out.println(LINE + "\n");
             } else {
-                String echo = "___________________________________\n" + "    " + userInput + "\n___________________________________";
+                taskList.add(userInput);
+                String echo = LINE + "\n" + "    added: " + userInput + "\n" + LINE + "\n";
+
+                if (commandCount > 5) {
+                    echo += "        Tard_T is tired...\n" + LINE + "\n";
+                }
                 System.out.println(echo);
             }
         }
-
-
-
     }
 }
