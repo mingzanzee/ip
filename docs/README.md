a65af95959a342c36f6f40cb2b9c67e6d11c4cfc
Zee Ming Zan <mingzan0210@gmail.com>
2026-08-17 13:33:24 +0800

    Add greeting and exit

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Tard_T User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Tard_T.java
@@ -0,0 +1,12 @@
+public class Tard_T {
+    public static void main(String[] args) {
+        String intro = "Hello! I'm Tard_T. \n" +
+                "What can I do for you? \n" +
+                "________________________________";
+        System.out.println(intro);
+
+        String exit = "Bye. Hope to see you again soon! \n" +
+                "________________________________";
+        System.out.println(exit);
+    }
+}
